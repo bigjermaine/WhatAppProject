59dd01bdb776023629d2c8fd6f8a14e8f3620ac2
jermaine-iris <danieljermaine97@gmail.com>
2025-04-15 17:05:04 +0100

    Initial Commit

---
+++ WhatAppProject/WhatAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatAppProjectApp.swift
+//  WhatAppProject
+//
+//  Created by Daniel Jermaine on 15/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhatAppProjectUITests/WhatAppProjectUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  WhatAppProjectUITestsLaunchTests.swift
+//  WhatAppProjectUITests
+//
+//  Created by Daniel Jermaine on 15/04/2025.
+//
+
+import XCTest
+
+final class WhatAppProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
