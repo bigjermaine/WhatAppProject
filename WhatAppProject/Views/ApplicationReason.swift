0a05ee226de37bb959e5dd98137e2e6c7a08760c
jermaine-iris <danieljermaine97@gmail.com>
2025-05-31 21:12:57 +0100

    nice

--- WhatAppProject/Views/ApplicationReason.swift
+++ WhatAppProject/Views/ApplicationReason.swift
@@ -22,7 +22,7 @@ enum ApplicationReason: String, CaseIterable, Identifiable {
         case .lostPassport:
             return  "Lost"
         case .pendingApplication:
-            return  ";lost"
+            return  "jermainez"
         }
     }
     var id: String { self.rawValue }
