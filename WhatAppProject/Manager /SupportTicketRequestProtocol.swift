09d93e4488c31b7f909210159474a95595e362d3
jermaine-iris <danieljermaine97@gmail.com>
2025-06-07 07:21:50 +0100

    feature(uploadDocumentTypes): request body functionality

--- WhatAppProject/Manager /SupportTicketRequestProtocol.swift
+++ WhatAppProject/Manager /SupportTicketRequestProtocol.swift
@@ -118,7 +118,9 @@ class SupportTicketService: SupportTicketRequestProtocol {
             throw NetworkError.serverError(statusCode: httpResponse.statusCode)
         }
     }
-    func uploadDocumentTypes(docomnets:DocumentList) async throws -> DocumentList {
+    
+    
+    func uploadDocumentTypes(documents: DocumentList) async throws -> DocumentList {
         let urlString = "\(domain)/api/document-service/document/create-documents"
         
         guard let url = URL(string: urlString) else {
@@ -129,6 +131,17 @@ class SupportTicketService: SupportTicketRequestProtocol {
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        // Encode the documents and set them as the request body
+        do {
+            let encoder = JSONEncoder()
+            encoder.keyEncodingStrategy = .convertToSnakeCase // If your backend expects snake_case
+            request.httpBody = try encoder.encode(documents)
+        } catch {
+            print("Encoding error: \(error)")
+           
+        }
+        
         let (data, response) = try await urlSession.data(for: request)
         guard let httpResponse = response as? HTTPURLResponse else {
             throw NetworkError.invalidResponse
@@ -138,9 +151,9 @@ class SupportTicketService: SupportTicketRequestProtocol {
         case 200...299:
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
-              do {
-                let data =  try decoder.decode(DocumentList.self, from: data)
-                return data
+            do {
+                let responseData = try decoder.decode(DocumentList.self, from: data)
+                return responseData
             } catch {
                 print("Decoding error: \(error)")
                 throw NetworkError.decodingError
@@ -149,7 +162,6 @@ class SupportTicketService: SupportTicketRequestProtocol {
         case 401:
             throw NetworkError.unauthorized
         case 400...499:
-            
             let errorMessage = try? JSONDecoder().decode(APIErrorResponse.self, from: data)
             throw NetworkError.serverError(statusCode: errorMessage?.code ?? 400)
             
@@ -157,7 +169,6 @@ class SupportTicketService: SupportTicketRequestProtocol {
             throw NetworkError.serverError(statusCode: httpResponse.statusCode)
         }
     }
-   
    
     
     func getEligiblity(docNo: String, birthdate: String, appReason: String) async throws -> ETCDataResponse {
