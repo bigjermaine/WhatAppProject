afc2ef93f18a78a18d220b1b6ca2790cb24edf32
jermaine-iris <danieljermaine97@gmail.com>
2025-09-06 09:30:24 +0100

    just there
    merging

---
+++ WhatAppProject/TestFirebaseAi/FirebaseAI.swift
@@ -0,0 +1,18 @@
+//
+//  FirebaseAI.swift
+//  WhatAppProject
+//
+//  Created by Daniel Jermaine on 05/09/2025.
+//
+
+import SwiftUI
+
+struct FirebaseAI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirebaseAI()
+}
