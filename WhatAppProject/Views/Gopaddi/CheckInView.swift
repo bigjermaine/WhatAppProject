7dbddad13eb8b31fbac7eec8b021b77e746a30e9
jermaine-iris <danieljermaine97@gmail.com>
2025-09-10 12:29:50 +0100

    Update CheckInView.swift
    jermaine

--- WhatAppProject/Views/Gopaddi/CheckInView.swift
+++ WhatAppProject/Views/Gopaddi/CheckInView.swift
@@ -6,6 +6,11 @@
 
 import SwiftUI
 
+enum StateUploadCheckIn{
+    case selected
+    case success
+    case failed
+}
 
 struct CheckInView: View {
     @Environment(\.dismiss) var dismiss
@@ -14,6 +19,7 @@ struct CheckInView: View {
     @State private var isUploading = false
     @State private var uploadFailed = false
     let idTypes = ["National ID card", "Drivers license", "Passport"]
+    @State var stateUploadCheckIn:StateUploadCheckIn = .selected
     var body: some View {
         VStack(alignment: .leading) {
             HStack {
@@ -27,8 +33,9 @@ struct CheckInView: View {
                     .font(.headline)
                 Spacer()
             }
+            .padding()
             Divider()
-                .padding(5)
+                .padding(.horizontal,5)
             VStack(spacing:20){
                 // Info Text
                 Text("Before we can hand over your room key, we just need to confirm your identity. Please upload a valid photo ID (passport, driver’s license, or government-issued ID).")
@@ -64,19 +71,16 @@ struct CheckInView: View {
                     }
                 }
                 
-                // File Upload Section (shown only when ID is selected)
                 if selectedID != nil {
                     FileUploadView(
                         selectedFile: $selectedFile,
                         isUploading: $isUploading,
-                        uploadFailed: $uploadFailed, selectedID: $selectedID)
+                        uploadFailed: $uploadFailed, selectedID: $selectedID, stateUploadCheckIn: $stateUploadCheckIn)
+                    .padding(.top)
                     
                 }
                 
                 Spacer()
-                
-                // Bottom Buttons
-                // Bottom Buttons
                 HStack(spacing: 16) {
                     Button(action: {
                         dismiss()
@@ -103,14 +107,18 @@ struct CheckInView: View {
                             .foregroundColor(selectedID == nil || selectedFile == nil ? .gray : .white)
                             .cornerRadius(12)
                     }
-                    .disabled(selectedID == nil || selectedFile == nil)
+                    .disabled(selectedFile == nil)
                 }
             }
             .padding(.top)
+            .padding()
         }
-        .padding()
-    }
-}
+        
+            }
+        }
+     
+
+
 
 #Preview {
     CheckInView()
@@ -120,54 +128,71 @@ struct FileUploadView: View {
     @Binding var selectedFile: URL?
     @Binding var isUploading: Bool
     @Binding var uploadFailed: Bool
-    @Binding  var selectedID: String?
+    @Binding var selectedID: String?
+    @Binding var stateUploadCheckIn:StateUploadCheckIn
     var body: some View {
-        VStack(spacing: 10) {
-            if selectedID != nil {
-                Image(systemName: "arrow.up.circle.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(Color.gray)
-                
-                Text("Choose a file")
-                    .font(.headline)
-                    .foregroundColor(Color.black)
-                
-                Text("JPEG, PNG, PDF, and MP4 formats, up to 50 MB.")
-                    .font(.caption)
-                    .foregroundColor(Color.gray)
-                
-                Button(action: {
-                    isUploading = true
-                    uploadFailed = false
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                        selectedFile = URL(string: "file://id_card.png")
+        VStack(alignment:.leading) {
+            Text("Upload \(selectedID ?? "")")
+            VStack(spacing: 10) {
+                if selectedID != nil {
+                    Image(systemName: "arrow.up.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(Color.gray)
+                    
+                    Text("Choose a file")
+                        .font(.headline)
+                        .foregroundColor(Color.black)
+                    
+                    Text("JPEG, PNG, PDF, and MP4 formats, up to 50 MB.")
+                        .font(.caption)
+                        .foregroundColor(Color.gray)
+                    
+                    Button(action: {
+                        isUploading = true
+                        uploadFailed = false
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            selectedFile = URL(string: "file://id_card.png")
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                stateUploadCheckIn = .selected
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                    stateUploadCheckIn = .failed
+                                    
+                                }
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
+                                    
+                                    stateUploadCheckIn = .success
+                                    
+                                    
+                                }
+                                
+                            }
+                        }
+                    }) {
+                        Text("Browse file")
+                            .padding(8)
                         
                     }
-                }) {
-                    Text("Browse file")
-                        .padding(8)
-                    
+                    .font(.subheadline)
+                    .background(Color.white)
+                    .foregroundColor(.black)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 4)
+                            .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                    )
+                    .cornerRadius(4)
                 }
-                .font(.subheadline)
-                .background(Color.white)
-                .foregroundColor(.black)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 4)
-                        .stroke(Color.gray.opacity(0.5), lineWidth: 1)
-                )
-                .cornerRadius(4)
             }
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.white)
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(Color.gray.opacity(0.8), style: StrokeStyle(lineWidth: 1, dash: [5]))
+            )
+            .cornerRadius(8)
         }
-        .frame(maxWidth: .infinity)
-        .padding()
-        .background(Color.white)
-        .overlay(
-            RoundedRectangle(cornerRadius: 8)
-                .stroke(Color.gray.opacity(0.8), style: StrokeStyle(lineWidth: 1, dash: [5]))
-        )
-        .cornerRadius(8)
         if  isUploading {
             HStack(spacing: 10) {
                 Image(systemName: "doc.fill")
@@ -180,30 +205,87 @@ struct FileUploadView: View {
                         
                     }
                     .foregroundColor(.black)
+                 
                     HStack{
-                      
-                        ProgressView()
-                            .progressViewStyle(CircularProgressViewStyle())
-                            .frame(width: 13, height: 13)
-                           
-                        Text("0 KB of 120 KB • \(isUploading ? "Uploading..." : "")")
-                            .font(.caption)
-                            .foregroundColor(.gray)
+                        switch stateUploadCheckIn {
+                        case .selected:
+                            Text("0 KB of 120 KB • \(isUploading ? "Uploading..." : "")")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                            ProgressView()
+                                .progressViewStyle(CircularProgressViewStyle())
+                                .frame(width: 13, height: 13)
+                        case .success:
+                         Text("0 KB of 120 KB •")
+                             .font(.caption)
+                             .foregroundColor(.gray)
+                            
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundColor(.green)
+                                .font(.system(size: 20))
+                                Text("Completed")
+                                .font(.caption)
+                        case .failed:
+                            Text("0 KB of 120 KB • ")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                            Image(systemName: "xmark.octagon.fill") // Failed icon
+                                .foregroundColor(.red)
+                                .font(.system(size: 20))
+                                Text("Failed")
+                                .font(.caption)
+                        }
+                        
+            
                     }
-                    ProgressView(value: 0.04)
-                        .tint(.blue)
-                        .frame(height: 10)
+                    switch stateUploadCheckIn {
+                    case.failed:
+                       Text("Try Again")
+                            .underline()
+                            .foregroundStyle(.red)
+                    case.selected:
+                        ProgressView(value: 0.04)
+                            .tint(.blue)
+                            .frame(height: 10)
+                    case.success:
+                        EmptyView()
+                    }
+                  
                     
                 }
                 
                 Spacer()
-                Image(systemName: "xmark.circle.fill")
-                    .resizable()
-                    .foregroundColor(.gray)
-                    .opacity(0.5)
-                    .frame(width: 20, height: 20,alignment: .topTrailing)
-                    .background(Color.white)
-                    .padding(.bottom,40)
+                switch stateUploadCheckIn {
+                case.failed:
+                    Image(systemName: "trash.fill")
+                        .resizable()
+                        .foregroundColor(.red)
+                        .opacity(0.5)
+                        .frame(width: 20, height: 20, alignment: .topTrailing)
+                        .background(Color.white)
+                        .clipShape(Circle()) // keeps it neat
+                        .padding(.bottom, 40)
+
+                case.selected:
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .foregroundColor(.gray)
+                        .opacity(0.5)
+                        .frame(width: 20, height: 20,alignment: .topTrailing)
+                        .background(Color.white)
+                        .padding(.bottom,40)
+                case.success:
+                    Image(systemName: "trash.fill")
+                        .resizable()
+                        .foregroundColor(.gray)
+                        .opacity(0.5)
+                        .frame(width: 20, height: 20, alignment: .topTrailing)
+                        .background(Color.white)
+                        .clipShape(Circle()) // keeps it neat
+                        .padding(.bottom, 40)
+
+                }
+              
             }
             .frame(height: 94
             )
@@ -211,7 +293,7 @@ struct FileUploadView: View {
             .background(Color.white)
             .overlay(
                 RoundedRectangle(cornerRadius: 12)
-                    .stroke(Color.gray.opacity(0.3), lineWidth: 1)
+                    .stroke(stateUploadCheckIn == .failed ? Color.red :  Color.gray.opacity(0.3), lineWidth: 1)
             )
             .cornerRadius(8)
             
